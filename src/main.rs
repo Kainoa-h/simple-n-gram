b6e7e0419690b506527e82225de8af5d76d60b3c
Kainoa-h <kainoahowj@gmail.com>
2025-06-05 20:17:50 +0800

    change: moved from 2 to 4 space indent... lol

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use simple_n_gram::*;
 
 fn main() {
-  println!("Hello, world!");
-  let config = Config::default();
-  let mut model = LidstoneModel::new(config);
-  let mut first_pp = LowerCasePreProcessor::new();
-  let pp2 = StartEndTokensPreProcessor::new();
-  first_pp.set_next(Box::new(pp2));
+    println!("Hello, world!");
+    let config = Config::default();
+    let mut model = LidstoneModel::new(config);
+    let mut first_pp = LowerCasePreProcessor::new();
+    let pp2 = StartEndTokensPreProcessor::new();
+    first_pp.set_next(Box::new(pp2));
 
-  let raw_corpus: String = std::fs::read_to_string("corpus.txt").expect("Failed to read corpus");
-  let corpus_vector = raw_corpus.split("\n").collect::<Vec<&str>>();
+    let raw_corpus: String = std::fs::read_to_string("corpus.txt").expect("Failed to read corpus");
+    let corpus_vector = raw_corpus.split("\n").collect::<Vec<&str>>();
 
-  model
-    .build_n_gram(Box::new(first_pp), corpus_vector)
-    .unwrap();
+    model
+        .build_n_gram(Box::new(first_pp), corpus_vector)
+        .unwrap();
 
-  match model.save() {
-    Ok(_) => println!("Model saved!"),
-    Err(err) => println!("{}", err),
-  }
+    match model.save() {
+        Ok(_) => println!("Model saved!"),
+        Err(err) => println!("{}", err),
+    }
 }
